1b5362800f0fb0ae997021761e2cd85ded3a04ed
NutsR <pymd.arc@gmail.com>
2021-11-15 19:23:01 +0530

    fixed crash on heroku maybe

--- next.config.js
+++ next.config.js
@@ -3,8 +3,22 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 	enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(
-	withPWA({
+if (process.env.NODE_ENV === "development") {
+	module.exports = withBundleAnalyzer(
+		withPWA({
+			reactStrictMode: true,
+
+			images: {
+				domains: ["cloudinary.com", "res.cloudinary.com"],
+			},
+			pwa: {
+				disable: process.env.NODE_ENV === "development",
+				dest: "public",
+			},
+		})
+	);
+} else {
+	module.exports = withPWA({
 		reactStrictMode: true,
 
 		images: {
@@ -14,5 +28,5 @@ module.exports = withBundleAnalyzer(
 			disable: process.env.NODE_ENV === "development",
 			dest: "public",
 		},
-	})
-);
+	});
+}
