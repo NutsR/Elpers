813492cf7e40c8ee7b048849857de9969cc055b1
NutsR <pymd.arc@gmail.com>
2021-11-15 19:26:26 +0530

    possible fix again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const withPWA = require("next-pwa");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-	enabled: process.env.ANALYZE === "true",
-});
-
 if (process.env.NODE_ENV === "development") {
+	const withBundleAnalyzer = require("@next/bundle-analyzer")({
+		enabled: process.env.ANALYZE === "true",
+	});
 	module.exports = withBundleAnalyzer(
 		withPWA({
 			reactStrictMode: true,
