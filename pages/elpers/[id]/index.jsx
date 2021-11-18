e09aa37b508b4bb368f413ee757586cf46a02736
NutsR <pymd.arc@gmail.com>
2021-11-18 17:59:22 +0530

    added fixes if you delete all Camps commonModule requires to es6 imports swr in [id] pages

--- pages/elpers/[id]/index.jsx
+++ pages/elpers/[id]/index.jsx
@@ -1,4 +1,5 @@
 import { getElperById } from "../../api/elpers/[id]";
+import useSWR, { SWRConfig } from "swr";
 import { useEffect, useState } from "react";
 import styles from "@/styles/id.module.scss";
 import { btn, btnInfo, btnDanger } from "@/styles/btn.module.scss";
@@ -10,7 +11,9 @@ const DetailedMap = dynamic(() => import("@/components/mapbox/details.map"), {
 	loading: () => <div className="loader"></div>,
 	ssr: false,
 });
-function PostDetails({ elpCamp }) {
+const fetcher = (url) => fetch(url).then((r) => r.json());
+function PostDetails({ fallback, id }) {
+	const { data, error } = useSWR(`/api/elpers/${id}`, fetcher);
 	const [size, setSize] = useState(true);
 	useEffect(() => {
 		if (typeof window !== "undefined") {
@@ -19,54 +22,64 @@ function PostDetails({ elpCamp }) {
 	}, []);
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.card}>
-				<div className={styles.imageCtrl}>
-					<Image
-						className={styles.image}
-						layout="fill"
-						src={elpCamp.images[0].url}
-						alt="post describing"
-					/>
-					<div className={styles.content}>
-						<h5 className={styles.title}>
-							{elpCamp.title} By {elpCamp.user.username}{" "}
-						</h5>
-						<span className={styles.textContent}>{elpCamp.description}</span>
-						<span className={styles.price}>{elpCamp.price}</span>
-						<span className={styles.location}>{elpCamp.location}</span>
+		<SWRConfig fallback={fallback}>
+			{!data ? (
+				<div className="overlay">
+					<div className="loader middle-load" />
+				</div>
+			) : error ? (
+				<div>error </div>
+			) : (
+				<div className={styles.container}>
+					<div className={styles.card}>
+						<div className={styles.imageCtrl}>
+							<Image
+								className={styles.image}
+								layout="fill"
+								src={data.images[0].url}
+								alt="post describing"
+							/>
+							<div className={styles.content}>
+								<h5 className={styles.title}>
+									{data.title} By {data.user.username}{" "}
+								</h5>
+								<span className={styles.textContent}>{data.description}</span>
+								<span className={styles.price}>{data.price}</span>
+								<span className={styles.location}>{data.location}</span>
+							</div>
+						</div>
+						<Link href="/elpers" passHref>
+							<button className={btn}>Go Back</button>
+						</Link>
+						<Link href={`/elpers/${data._id}/edit`} passHref>
+							<button className={btnInfo}>Edit</button>
+						</Link>
+						<button
+							className={btnDanger}
+							onClick={() => {
+								deleteMethod(data._id);
+							}}
+						>
+							Delete
+						</button>
+					</div>
+					<div className={styles.map}>
+						<DetailedMap
+							elpers={data}
+							width={size ? "36vw" : "45vw"}
+							height={size ? "40.6vh" : "23.2vh"}
+							token={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+						/>
+						<div className={styles.reviews}>
+							<h5 className={styles.title}>Reviews</h5>
+							{data.review.map((el) => (
+								<span key={el._id}>{el.review}</span>
+							))}
+						</div>
 					</div>
 				</div>
-				<Link href="/elpers" passHref>
-					<button className={btn}>Go Back</button>
-				</Link>
-				<Link href={`/elpers/${elpCamp._id}/edit`} passHref>
-					<button className={btnInfo}>Edit</button>
-				</Link>
-				<button
-					className={btnDanger}
-					onClick={() => {
-						deleteMethod(elpCamp._id);
-					}}
-				>
-					Delete
-				</button>
-			</div>
-			<div className={styles.map}>
-				<DetailedMap
-					elpers={elpCamp}
-					width={size ? "36vw" : "45vw"}
-					height={size ? "40.6vh" : "23.2vh"}
-					token={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-				/>
-				<div className={styles.reviews}>
-					<h5 className={styles.title}>Reviews</h5>
-					{elpCamp.review.map((el) => (
-						<span key={el._id}>{el.review}</span>
-					))}
-				</div>
-			</div>
-		</div>
+			)}
+		</SWRConfig>
 	);
 }
 
@@ -79,7 +92,10 @@ export async function getServerSideProps({ params: { id }, req, res }) {
 		const elpCamp = await getElperById(id);
 		return {
 			props: {
-				elpCamp,
+				id,
+				fallback: {
+					[`/api/elpers/${id}`]: elpCamp,
+				},
 			},
 		};
 	} catch (err) {
