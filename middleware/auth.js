e8b07527bbe0a3eb55db36e14f5d7597dc620928
NutsR <68601880+NutsR@users.noreply.github.com>
2021-11-19 23:02:44 +0530

    Update auth.js
    Oops

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,7 +21,7 @@ const auth = nextConnect()
 	.use(passport.initialize())
 	.use(passport.session());
 
-passport.use(user.createStrategy());
+passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 export default auth;
