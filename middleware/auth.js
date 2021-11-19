7d2eda1eaba359175fb0d7c029c5425b6c0abcb1
NutsR <pymd.arc@gmail.com>
2021-11-19 23:15:06 +0530

    uh mistakes

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,7 +21,7 @@ const auth = nextConnect()
 	.use(passport.initialize())
 	.use(passport.session());
 
-passport.use(User.createStrategy());
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 export default auth;
