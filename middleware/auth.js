3e7b922fbf24fd277b0ab7698d0e8cd6793d4999
NutsR <pymd.arc@gmail.com>
2021-11-20 13:03:34 +0530

    token fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,10 +8,10 @@ const auth = nextConnect()
 	.use(
 		session({
 			name: "sess",
-			secret: process.env.TOKEN_SECRET || "unsafesecret",
+			secret: process.env.TOKEN_SECRET || "unsafesecretmustchangetogoodone",
 			cookie: {
 				maxAge: 60 * 60 * 8, // 8 hours,
-				httpOnly: process.env.NODE_ENV === "development",
+				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
 				path: "/",
 				sameSite: "lax",
